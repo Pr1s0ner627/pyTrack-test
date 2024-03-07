42d531d0239d4a97ec5fb3ac2bb9f7d42810e28f
Pr1s0ner627 <g1mw9827@gmail.com>
2024-03-07 15:14:43 +0530

    Next Commit

--- first.py
+++ first.py
@@ -4,7 +4,16 @@ def listCreate(lst,n):
         lst.append(num)
     return lst
 
+
+def listRemove(lst):
+    inp=input("Delete the List [Y/N] : ")
+    if inp in 'yY':
+        lst.remove()
+    return lst
+
+
 lst=[]
 lim=int(input("Enter the Limit : "))
 
 listCreate(lst,lim)
+listRemove(lst)
