aa0dd97e325736cc52a038b63373d3ac439d90a3
Pr1s0ner627 <g1mw9827@gmail.com>
2024-03-15 14:52:22 +0530

    Revert "Added Updation"
    This reverts commit afda0000b8c17d697869545e818d663f8f874860.

--- first.py
+++ first.py
@@ -9,23 +9,10 @@ def listRemove(lst):
     inp=input("Delete the List [Y/N] : ")
     if inp in 'yY':
         lst.remove()
-    else:
-        pass
-
-def listUpdate(lst):
-    inp=input("Want to update the value : ")
-    if inp in 'yY':
-        idx=input("Enter the Position you want to update : ")
-        if(idx>=lim):
-            print("Try Again!")
-        else:
-            num=int(input("Enter input : "))
-            lst[idx]=num
-    else:
-        pass
     return lst
-
-
+    else:
+        pass
+        
 lst=[]
 lim=int(input("Enter the Limit : "))
 
