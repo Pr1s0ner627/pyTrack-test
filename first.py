d735796023a49df6352fa5ab5e9e0c11b0148e03
Pr1s0ner627 <g1mw9827@gmail.com>
2024-03-15 14:53:33 +0530

    Multiple Ops Needed

--- first.py
+++ first.py
@@ -9,10 +9,23 @@ def listRemove(lst):
     inp=input("Delete the List [Y/N] : ")
     if inp in 'yY':
         lst.remove()
-    return lst
     else:
         pass
-        
+
+def listUpdate(lst):
+    inp=input("Want to update the value : ")
+    if inp in 'yY':
+        idx=input("Enter the Position you want to update : ")
+        if(idx>=lim):
+            print("Try Again!")
+        else:
+            num=int(input("Enter input : "))
+            lst[idx]=num
+    else:
+        pass
+    return lst
+
+
 lst=[]
 lim=int(input("Enter the Limit : "))
 
